c5bb42f1d09bd247811b3537db9f49ab9a684aff
ray.rahul.8101 <ray.rahul.8101@gmail.com>
2026-01-15 09:43:26 +0000

    cc

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
-    System.out.println("Enter an Integer");
-    int a = myObj.nextInt();
-    System.out.println("Hello World " + a);
+                          
   }
 
 }
---
+++ src/main/java/com/example/learning/zfunction.java
@@ -0,0 +1,10 @@
+package com.example.learning;
+
+import java.util.Scanner;
+
+public class zfunction {
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Enter an Integer");
+    int a = myObj.nextInt();
+    System.out.println("Hello World " + a);
+}
