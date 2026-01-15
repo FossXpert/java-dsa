3382992510d376e16f53a53e539a8c93d675ee7e
ray.rahul.8101 <ray.rahul.8101@gmail.com>
2026-01-15 08:48:40 +0000

    HELLO

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,15 @@
 package com.example.demo;
+import java.util.Scanner;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
-
-  @Value("${NAME:World}")
-  String name;
-
-  @RestController
-  class HelloworldController {
-    @GetMapping("/")
-    String hello() {
-      return "Hello " + name + "!";
-    }
-  }
-
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Enter an Integer");
+    int a = myObj.nextInt();
+    System.out.println("Hello World " + a);
   }
 
 }
