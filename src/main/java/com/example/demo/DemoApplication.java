76e8cf050eff3126283c593d6af97c46ea35e33b
ray.rahul.8101 <ray.rahul.8101@gmail.com>
2026-01-15 10:51:24 +0000

    RR

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,13 @@
 package com.example.demo;
-import java.util.Scanner;
 
+import com.example.learning.zfunction;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
-                          
+    zfunction z = new zfunction();
+    z.run();
   }
 
 }
--- src/main/java/com/example/learning/zfunction.java
+++ src/main/java/com/example/learning/zfunction.java
@@ -3,8 +3,19 @@ package com.example.learning;
 import java.util.Scanner;
 
 public class zfunction {
-    Scanner myObj = new Scanner(System.in);
-    System.out.println("Enter an Integer");
-    int a = myObj.nextInt();
-    System.out.println("Hello World " + a);
+
+    public void calcAns(String inp){
+        System.out.println("input string is " + inp);
+        int arr[];
+
+        for(int i=0;i<inp.length();i++){
+            
+        }
+
+    }
+    public void run() {
+        Scanner myObj = new Scanner(System.in);
+        String inp = myObj.nextLine();
+        calcAns(inp);
+    }
 }
