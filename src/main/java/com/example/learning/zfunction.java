0def2bfccd27fc2b34c62a7ebba542597272bd8e
ray.rahul.8101 <ray.rahul.8101@gmail.com>
2026-01-15 15:01:05 +0000

    ccc

--- src/main/java/com/example/learning/zfunction.java
+++ src/main/java/com/example/learning/zfunction.java
@@ -1,5 +1,6 @@
 package com.example.learning;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class zfunction {
@@ -7,11 +8,25 @@ public class zfunction {
     public void calcAns(String inp){
         System.out.println("input string is " + inp);
         int arr[];
-
-        for(int i=0;i<inp.length();i++){
-            
+        int n = inp.length();
+        arr = new int[n];
+        for(int i=0;i<n;i++){
+            int cnt = 0;
+            int l =0;
+            int j=i;
+            for (;j<n;){
+                if(inp.charAt(l) != inp.charAt(j)){
+                    System.out.println(inp.charAt(l)+ "" + inp.charAt(j));
+                    break;
+                }
+                System.out.println(inp.charAt(l)+ "-" + inp.charAt(j));
+                l++;j++;++cnt;
+            }
+            System.out.println("cnt-"  + cnt);
+            arr[i] = cnt;
         }
-
+        arr[0] = 0;
+        System.out.println("ans : " + Arrays.toString(arr));
     }
     public void run() {
         Scanner myObj = new Scanner(System.in);
